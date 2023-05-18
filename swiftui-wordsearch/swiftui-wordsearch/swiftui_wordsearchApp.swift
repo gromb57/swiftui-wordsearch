64888ff075410d92b1aa7258ffc7b33247f8b4c1
Bach Jérôme <jeromeb57@gmail.com>
2023-05-18 18:43:14 +0200

    init

---
+++ swiftui-wordsearch/swiftui-wordsearch/swiftui_wordsearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_wordsearchApp.swift
+//  swiftui-wordsearch
+//
+//  Created by Jerome Bach on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_wordsearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
